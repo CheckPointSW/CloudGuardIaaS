6c662f4d835cdd16fc209295fe477a9af49b6e5a
Amit Schnitzer <18722911+amit-schnitzer@users.noreply.github.com>
2020-04-16 19:09:28 +0300

    contrib | azure terraform template to add peering on top of the vmss vnet creation

---
+++ contrib/terraform/azure/vmss-new-vnet-with-peer/cloud-init.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/python3 /etc/cloud_config.py
+
+installationType="${installation_type}"
+allowUploadDownload="${allow_upload_download}"
+osVersion= "${os_version}"
+templateName="${template_name}"
+templateVersion="${template_version}"
+isBlink="${is_blink}"
+bootstrapScript64="${bootstrap_script64}"
+location="${location}"
+sicKey="${sic_key}"
+vnet="${vnet}"
