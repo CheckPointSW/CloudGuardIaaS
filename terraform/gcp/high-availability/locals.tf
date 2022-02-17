f663f498ba5b1588e42acba49e7f13a4cdd10c01
itaysu <itaysu@checkpoint.com>
2022-02-17 14:25:27 +0200

    Fix image validation

--- terraform/gcp/high-availability/locals.tf
+++ terraform/gcp/high-availability/locals.tf
@@ -5,9 +5,9 @@ locals {
   // will fail if [var.license] is invalid:
   validate_license = index(local.license_allowed_values, upper(var.license))
 
-  split_image_name = split("-", var.image_name)
-  // will fail if the image license name is unmatched to var.license:
-  validate_image_name = index(local.split_image_name, lower(var.license)) == var.license && index(local.split_image_name, "cluster") == "cluster" ? 0 : "invalid image name or license - var.image_name must contain var.license (byol/payg) and 'cluster' license type."
+  regex_validate_image_name = "check-point-r8[0-1][1-4]0-gw-(byol|payg)-cluster-[0-9]{3}-([0-9]{3}|[a-z]+)-v[0-9]{8,}"
+  // will fail if the image name is not in the right syntax
+  validate_image_name = length(regexall(local.regex_validate_image_name, var.image_name)) > 0 ? 0 : index(split("-", var.image_name), "INVALID IMAGE NAME")
 
   split_zoneA = split("-", var.zoneA)
   split_zoneB = split("-", var.zoneB)
