53a7114f1183456cf2e302ff3228ece2fed7e6a0
yairra <yairra@checkpoint.com>
2023-07-19 15:11:49 +0300

    Terraform GCP | Added Single GW into new VPC template

--- terraform/gcp/single-into-new-vpc/output.tf
+++ terraform/gcp/single-into-new-vpc/output.tf
@@ -10,3 +10,21 @@ output "internal_network" {
 output "internal_subnetwork" {
   value = google_compute_subnetwork.internal_subnetwork.name
 }
+output "SIC_key" {
+  value = module.single-into-existing-vpc.SIC_key
+}
+output "ICMP_firewall_rules_name" {
+  value = module.single-into-existing-vpc.ICMP_firewall_rules_name
+}
+output "TCP_firewall_rules_name" {
+  value = module.single-into-existing-vpc.TCP_firewall_rules_name
+}
+output "UDP_firewall_rules_name" {
+  value = module.single-into-existing-vpc.UDP_firewall_rules_name
+}
+output "SCTP_firewall_rules_name" {
+  value = module.single-into-existing-vpc.SCTP_firewall_rules_name
+}
+output "ESP_firewall_rules_name" {
+  value = module.single-into-existing-vpc.ESP_firewall_rules_name
+}
