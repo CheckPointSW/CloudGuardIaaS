5a59b9892fa97b19cf4e19a04011a3a44f5f79cf
yairra <yairra@checkpoint.com>
2023-12-14 14:16:16 +0200

    Added Azure vWAN TF templates

---
+++ terraform/azure/nva-into-existing-hub/variables.tf
@@ -0,0 +1,139 @@
+variable "authentication_method" {
+  description = "Azure authentication method"
+  type = string
+  validation {
+    condition     = contains(["Azure CLI", "Service Principal"], var.authentication_method)
+    error_message = "Valid values for authentication_method are 'Azure CLI','Service Principal'"
+  }
+}
+
+variable "subscription_id" {
+  description = "Subscription ID"
+  type = string
+}
+
+variable "tenant_id" {
+  description = "Tenant ID"
+  type = string
+}
+
+variable "client_id" {
+  description = "Application ID(Client ID)"
+  type = string
+}
+
+variable "client_secret" {
+  description = "A secret string that the application uses to prove its identity when requesting a token. Also can be referred to as application password."
+  type = string
+}
+
+variable "resource-group-name" {
+  type    = string
+  default = "tf-managed-app-resource-group"
+}
+
+variable "location" {
+  type    = string
+  default = "westcentralus"
+}
+
+variable "managed-app-name" {
+  type    = string
+  default = "tf-vwan-managed-app-nva"
+}
+
+variable "vwan-hub-name" {
+  type    = string
+}
+
+variable "vwan-hub-resource-group" {
+  type    = string
+}
+
+variable "nva-rg-name" {
+  type    = string
+  default = "tf-vwan-nva-rg"
+}
+
+variable "nva-name" {
+  type    = string
+  default = "tf-vwan-nva"
+}
+
+variable "cloudguard-version" {
+  type    = string
+  default = "R81.10 - Pay As You Go (NGTP)"
+  validation {
+    condition     = contains(["R81.10 - Pay As You Go (NGTP)", "R81.20 - Pay As You Go (NGTP)", "R81.10 - Pay As You Go (NGTX)", "R81.20 - Pay As You Go (NGTX)"], var.cloudguard-version)
+    error_message = "Valid values for CloudGuard version are 'R81.10 - Pay As You Go (NGTP)','R81.20 - Pay As You Go (NGTP)','R81.10 - Pay As You Go (NGTX)' and 'R81.20 - Pay As You Go (NGTX)'"
+  }
+}
+
+variable "scale-unit" {
+  type    = string
+  default = "2"
+}
+
+variable "bootstrap-script" {
+  type    = string
+  default = ""
+}
+
+variable "admin-shell" {
+  type    = string
+  default = "/etc/cli.sh"
+  validation {
+    condition     = contains(["/etc/cli.sh", "/bin/bash", "/bin/tcsh", "/bin/csh"], var.admin-shell)
+    error_message = "Valid shells are '/etc/cli.sh', '/bin/bash', '/bin/tcsh', '/bin/csh'"
+  }
+}
+
+variable "sic-key" {
+  type      = string
+  default   = ""
+  sensitive = true
+}
+
+variable "ssh-public-key" {
+  type    = string
+  default = ""
+}
+
+variable "bgp-asn" {
+  type    = string
+  default = "64512"
+}
+
+variable "custom-metrics" {
+  type    = string
+  default = "yes"
+  validation {
+    condition     = contains(["yes", "no"], var.custom-metrics)
+    error_message = "Valid options are string('yes' or 'no')"
+  }
+}
+
+variable "smart1-cloud-token-a" {
+  type    = string
+  default = ""
+}
+
+variable "smart1-cloud-token-b" {
+  type    = string
+  default = ""
+}
+
+variable "smart1-cloud-token-c" {
+  type    = string
+  default = ""
+}
+
+variable "smart1-cloud-token-d" {
+  type    = string
+  default = ""
+}
+
+variable "smart1-cloud-token-e" {
+  type    = string
+  default = ""
+}
---
+++ terraform/azure/nva-into-new-hub/main.tf
@@ -0,0 +1,129 @@
+//********************** Basic Configuration **************************//
+resource "azurerm_resource_group" "managed-app-rg" {
+  name     = var.resource-group-name
+  location = var.location
+}
+
+resource "azurerm_virtual_wan" "vwan" {
+  name                = var.vwan-name
+  resource_group_name = azurerm_resource_group.managed-app-rg.name
+  location            = var.location
+}
+
+resource "azurerm_virtual_hub" "vwan-hub" {
+  name                = var.vwan-hub-name
+  resource_group_name = azurerm_resource_group.managed-app-rg.name
+  location            = azurerm_resource_group.managed-app-rg.location
+  address_prefix      = var.vwan-hub-address-prefix
+  virtual_wan_id      = azurerm_virtual_wan.vwan.id
+}
+
+
+//********************** Image Version **************************//
+
+data "external" "az_access_token" {
+  count = var.authentication_method == "Azure CLI" ? 1 : 0
+  program = ["az", "account", "get-access-token", "--resource=https://management.azure.com", "--output=json"]
+}
+
+data "http" "azure_auth" {
+  count = var.authentication_method == "Service Principal" ? 1 : 0
+  url    = "https://login.microsoftonline.com/${var.tenant_id}/oauth2/v2.0/token"
+  method = "POST"
+  request_headers = {
+    "Content-Type" = "application/x-www-form-urlencoded"
+  }
+  request_body = "grant_type=client_credentials&client_id=${var.client_id}&client_secret=${var.client_secret}&scope=https://management.azure.com/.default"
+}
+
+locals {
+  access_token = var.authentication_method == "Service Principal" ? jsondecode(data.http.azure_auth[0].response_body).access_token : data.external.az_access_token[0].result.accessToken
+}
+
+data "http" "image-versions" {
+  method = "GET"
+  url = "https://management.azure.com/subscriptions/${var.subscription_id}/providers/Microsoft.Network/networkVirtualApplianceSKUs/checkpoint${strcontains(var.cloudguard-version, "NGTX") ? "-ngtx" : ""}?api-version=2020-05-01"
+  request_headers = {
+    Accept = "application/json"
+    "Authorization" = "Bearer ${local.access_token}"
+  }
+}
+
+locals {
+      image_versions = tolist([for version in jsondecode(data.http.image-versions.response_body).properties.availableVersions : version if substr(version, 0, 4) == lower(substr(replace(var.cloudguard-version, ".", ""), 1, 4))])
+}
+
+//********************** Managed Application Configuration **************************//
+resource "azurerm_managed_application" "nva" {
+  name                        = var.managed-app-name
+  location                    = azurerm_resource_group.managed-app-rg.location
+  resource_group_name         = azurerm_resource_group.managed-app-rg.name
+  kind                        = "MarketPlace"
+  managed_resource_group_name = var.nva-rg-name
+
+  plan {
+    name      = "vwan-app"
+    product   = "azure-vwan"
+    publisher = "checkpoint"
+    version   = "1.0.7"
+  }
+  parameter_values = jsonencode({
+    location = {
+      value = azurerm_resource_group.managed-app-rg.location
+    },
+    hubId = {
+      value = azurerm_virtual_hub.vwan-hub.id
+    },
+    cloudGuardVersion = {
+      value = var.cloudguard-version
+    },
+    imageVersion = {
+      value = element(local.image_versions, length(local.image_versions) -1)
+    },
+    scaleUnit = {
+      value = var.scale-unit
+    },
+    bootstrapScript = {
+      value = var.bootstrap-script
+    },
+    adminShell = {
+      value = var.admin-shell
+    },
+    sicKey = {
+      value = var.sic-key
+    },
+    sshPublicKey = {
+      value = var.ssh-public-key
+    },
+    BGP = {
+      value = var.bgp-asn
+    },
+    NVA = {
+      value = var.nva-name
+    },
+    customMetrics = {
+      value = var.custom-metrics
+    },
+    hubASN = {
+      value = azurerm_virtual_hub.vwan-hub.virtual_router_asn
+    },
+    hubPeers = {
+      value = azurerm_virtual_hub.vwan-hub.virtual_router_ips
+    },
+    smart1CloudTokenA = {
+      value = var.smart1-cloud-token-a
+    },
+    smart1CloudTokenB = {
+      value = var.smart1-cloud-token-b
+    },
+    smart1CloudTokenC = {
+      value = var.smart1-cloud-token-c
+    },
+    smart1CloudTokenD = {
+      value = var.smart1-cloud-token-d
+    },
+    smart1CloudTokenE = {
+      value = var.smart1-cloud-token-e
+    }
+  })
+}
