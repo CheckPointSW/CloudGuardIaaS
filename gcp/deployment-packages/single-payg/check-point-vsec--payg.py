306644a26754bc8b2d1ff019061ebf7b12fff705
rivkas <rivkas@checkpoint.com>
2024-01-09 10:47:46 +0200

    reverse the mgmt-only modifications in single payg

--- gcp/deployment-packages/single-payg/check-point-vsec--payg.py
+++ gcp/deployment-packages/single-payg/check-point-vsec--payg.py
@@ -36,11 +36,6 @@ ATTRIBUTES = {
         'description': 'Check Point Security Gateway and Management',
         'canIpForward': True,
     },
-    'Management only': {
-        'tags': [MANAGEMENT],
-        'description': 'Check Point Security Management',
-        'canIpForward': False,
-    },
     'Gateway only': {
         'tags': [GATEWAY],
         'description': 'Check Point Security Gateway',
@@ -151,7 +146,7 @@ def generate_config(context):
     prop['osVersion'] = prop['cloudguardVersion'].replace(".", "")
     prop['allowUploadDownload'] = str(prop['allowUploadDownload']).lower()
     if not prop['managementGUIClientNetwork'] and prop['installationType'] in {
-            'Gateway and Management (Standalone)', 'Management only'}:
+            'Gateway and Management (Standalone)'}:
         raise Exception('Allowed GUI clients are required when installing '
                         'a management server')
     for k in ['managementGUIClientNetwork']:
