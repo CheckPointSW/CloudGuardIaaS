e112a0ac6db795045113a835e614216035ea2ad8
dmitrytc <dmitrytc@checkpoint.com>
2024-02-28 17:37:10 +0200

    Added ingress support

--- terraform/azure/nva-into-new-vwan/main.tf
+++ terraform/azure/nva-into-new-vwan/main.tf
@@ -177,6 +177,15 @@ resource "azurerm_managed_application" "nva" {
     },
     smart1CloudTokenE = {
       value = var.smart1-cloud-token-e
+    },
+    publicIPIngress = {
+      value = (var.new-public-ip == "yes" || length(var.existing-public-ip) > 0) ? "yes" : "no"
+    },
+    createNewIPIngress = {
+      value = var.new-public-ip
+    }
+    ipIngressExistingResourceId = {
+      value = var.existing-public-ip
     }
   })
 }
